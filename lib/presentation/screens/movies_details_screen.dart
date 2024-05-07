0e6050effb43ad73a5900fb9d07c8897e1fe3ec6
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-07 09:49:38 -0300

    feat: adiciona a tela de detalhes

---
+++ lib/presentation/screens/movies_details_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MovieDetailsScreen extends StatelessWidget {
+  const MovieDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Detalhes do Filme:'),
+      ),
+    );
+  }
+}
