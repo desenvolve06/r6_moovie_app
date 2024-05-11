d2b83b25be8774ac8c88b61a31a282aa2f04b79e
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-11 19:34:42 -0300

    feat: add list text and overview

--- lib/presentation/screens/movies_details_screen.dart
+++ lib/presentation/screens/movies_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../data/models/movies_model.dart';
 import '../components/details/movie_detail_header.dart';
+import '../components/details/overview..dart';
 
 class MovieDetailsScreen extends StatelessWidget {
   final dynamic item;
@@ -26,8 +27,8 @@ class MovieDetailsScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: MovieDetailHeader(movie: movie, height: 60),
+              padding: const EdgeInsets.only(bottom: 20),
+              child: MovieDetailHeader(movie: movie,  height: 60),
             ),
             Center(
               child: Padding(
@@ -37,26 +38,56 @@ class MovieDetailsScreen extends StatelessWidget {
                   child: Row(
                     children: [
                       Expanded(
-                        child: _buildInfoItem(
-                            Icons.date_range, movie.releaseDate.toString()),
+                        child: _buildInfoItem(Icons.date_range, movie.releaseDate.toString()),
                       ),
                       _buildDivider(),
                       Expanded(
-                        child: _buildInfoItem(
-                            Icons.movie, movie.popularity.toString()),
+                        child: _buildInfoItem(Icons.access_time, '120 min'),
                       ),
                       _buildDivider(),
                       Expanded(
-                        child: _buildInfoItem(
-                            Icons.category, movie.genreIds.toString()),
+                        child: _buildInfoItem(Icons.category, movie.genreIds.toString()),
                       ),
                     ],
                   ),
                 ),
               ),
             ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("About Movie"),
+                  Text("Review"),
+                  Text("Cast"),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: OverView(movie.overview),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildInfoItem(IconData icon, String text) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Icon(icon, color: Colors.grey.shade600),
+          const SizedBox(height: 8),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.grey),
+          ),
+        ],
       ),
     );
   }
@@ -71,24 +102,3 @@ class MovieDetailsScreen extends StatelessWidget {
     );
   }
 }
-
-Widget _buildInfoItem(IconData icon, String text) {
-  return Expanded(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Icon(icon, color: Colors.grey.shade600),
-          const SizedBox(height: 8),
-          Text(
-            text,
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: Colors.grey),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
-
