9829eaa4a45ce86238dad8c144c49e9ef525dc41
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-12 01:40:56 -0300

    feat:[R6-80/4] create text list details

---
+++ lib/presentation/components/details/text_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextList extends StatelessWidget {
+  final List<String> items;
+
+  const TextList({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          for (var item in items)
+            Text(
+              item,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+        ],
+      ),
+    );
+  }
+}
