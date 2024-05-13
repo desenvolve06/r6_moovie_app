06fa9ff45ec6f6c309d358c11204ad7f739782e6
Patrick Garcia <patrickvicentegarcia@gmail.com>
2024-05-13 19:55:06 -0300

    feat: [R6-86] Add constants

---
+++ lib/resources/app_strings.dart
@@ -0,0 +1,38 @@
+class AppStrings {
+  static const String appTitle = 'KoruFlix';
+  static const String details = 'Detalhes';
+  static const String seeAll = 'Ver todos';
+  static const String popularMovies = 'Filmes populares';
+  static const String topRatedMovies = 'Top Filmes avaliados';
+  static const String story = 'Story';
+  static const String videos = 'Videos';
+  static const String reviews = 'Reviews';
+  static const String similar = 'Similar';
+  static const String showLess = 'Mostrar menos';
+  static const String showMore = 'Mostrar mais';
+  static const String movies = 'Filmes';
+  static const String cast = 'Cast';
+  static const String shows = 'Shows';
+  static const String search = 'Buscar';
+  static const String watchlist = 'Watchlist';
+  static const String popularShows = 'Shows Populares';
+  static const String topRatedShows = 'Top Shows avaliados';
+  static const String lastEpisodeOnAir = 'Último Episódio no ar.';
+  static const String seasons = 'Temporadas';
+  static const String season = 'Temporada';
+  static const String episodes = 'Episódios';
+  static const String episode = 'Episódio';
+  static const String lastEpisode = 'Último episódio';
+  static const String searchText =
+      'By typing in search bar, Movia search in movies and series and then show you the best results.';
+  static const String searchHint = 'Search for Movies, Series...';
+  static const String watchlistIsEmpty = 'Watchlist está vazia';
+  static const String watchlistText =
+      'Depois de adicionar filmes e séries na watchlist, eles irão aparecer aqui.';
+  static const String oops = 'Ooops';
+  static const String tryAgainLater = 'Por favor, tente novamente mais tarde!';
+  static const String errorMessage = 'Algo está errado.';
+  static const String tryAgain = 'tente novamente';
+  static const String noResults = 'Sem resultados';
+  static const String aboutSerie = 'Sobre a série';
+}
