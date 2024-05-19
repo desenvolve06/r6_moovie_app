aa73c4f3123edee8914ada52d685e403a43330f9
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-19 12:43:31 -0300

    feat:[R6-76] Create movies service

---
+++ lib/data/service/movie_service_api.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import '../models/movies_response.dart';
+part 'movie_service_api.g.dart';
+
+@RestApi(baseUrl: 'https://api.themoviedb.org/3')
+abstract class MovieServiceApi {
+  factory MovieServiceApi(Dio dio, {String baseUrl}) = _MovieServiceApi;
+
+  @GET('/movie/popular')
+  Future<MoviesResponse> getPopular();
+}
