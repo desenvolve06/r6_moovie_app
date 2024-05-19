4e8c53ca3a6caaa3e4e813600b435900b5bd0027
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-19 12:01:32 -0300

    feat:[R6-76] Add usecase and entities layer domain

---
+++ lib/domain/usecase/get_popular_movie_use_case.dart
@@ -0,0 +1,10 @@
+import '../entities/movie.dart';
+import '../repository/movie_repository.dart';
+
+class GetPopularMoviesUseCase {
+  final MoviesRepository repository;
+
+  GetPopularMoviesUseCase(this.repository);
+
+  Future<List<Movie>> invoke() => repository.getPopularMovies();
+}
