dafcf5aad9c1138f42c68ba2c28d988cf7666b2a
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-05-23 20:20:45 -0300

    feat:Implement crud in data layer

--- lib/data/repository/movie_repository_impl.dart
+++ lib/data/repository/movie_repository_impl.dart
@@ -1,15 +1,40 @@
+import 'dart:convert';
+
 import '../../domain/entities/movie.dart';
 import '../../domain/repository/movie_repository.dart';
+import '../local/local_data_source.dart';
 import '../source/movie_data_source.dart';
 
 class MoviesRepositoryImpl implements MoviesRepository {
   final MovieDataSource moviesDataSource;
-  // final LocalDataSource localDataSource;
+  final LocalDataSource localDataSource;
 
-  MoviesRepositoryImpl(this.moviesDataSource);
+  MoviesRepositoryImpl(this.moviesDataSource, this.localDataSource);
 
   @override
   Future<List<Movie>> getPopularMovies() => moviesDataSource.getPopularMovies();
+
+  @override
+  Future<void> addToFavorites(int id) async {
+  await localDataSource.addToFavorites(id);
+  }
+
+  @override
+  Future<List<int>> getFavourites() async {
+   return await localDataSource.getFavourites();
+  }
+
+  @override
+  Future<bool> isFavourite(int id) {
+    // TODO: implement isFavourite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeFromFavorites(int id) async {
+    localDataSource.removeFromFavorites(id);
+  }
+
 
   // @override
   // List<int> getFavoritesMovies() => localDataSource.getFavouritesList();
