56bc71afb00de83816443f2a5242f25a57c1e242
arlannunesdev <arlan1.soares1@gmail.com>
2024-05-26 10:12:07 -0300

    Refactor login_page

--- lib/presenter/pages/login_page.dart
+++ lib/presenter/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import '../../main.dart';
 import '../../resources/app_colors.dart';
 import '../../resources/app_strings.dart';
@@ -10,77 +12,156 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   Color textColor = Colors.blue;
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          verticalDirection: VerticalDirection.down,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              alignment: Alignment.topCenter,
-              'assets/logoKoruFlix.png',
-              scale: 2.0,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)),
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0)),
-                labelText: 'Senha',
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            InkWell(
-              borderRadius: const BorderRadius.all(Radius.circular(50.0)),
-              onTap: () {
-                setState(() {
-                  textColor = const Color.fromARGB(255, 255, 24, 24);
-                });
-              },
-              child: const Text('Esqueceu a senha?',
-                  textAlign: TextAlign.end,
-                  style: TextStyle(
-                    color: Colors.blue,
-                  )),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
-                  backgroundColor: (AppColors.primaryColorDark)),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const MyHomePage(title: AppStrings.appTitle)),
-                );
-              },
-              child: const Text(
-                'Login',
-                style: TextStyle(color: AppColors.primaryText),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Login'),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            verticalDirection: VerticalDirection.down,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                alignment: Alignment.topCenter,
+                'assets/logoKoruFlix.png',
+                scale: 2.0,
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  hintText: 'Email',
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(10.0),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    hintText: 'Senha',
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: _togglePasswordVisibility,
+                    )),
+              ),
+              const SizedBox(height: 10.0),
+              GestureDetector(
+                onTap: () {},
+                child: const Text('Esqueceu a senha?',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      color: Colors.blue,
+                    )),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)),
+                    backgroundColor: (AppColors.primaryColorDark)),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const MyHomePage(title: AppStrings.appTitle)),
+                  );
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: AppColors.primaryText),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.primaryText,
+                      height: 50,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text('OU'),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: AppColors.primaryText,
+                      height: 50,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      print('Login com Facebook');
+                    },
+                    child: Image.asset(
+                      scale: 1.5,
+                      'assets/icon_face.png',
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  InkWell(
+                    onTap: () {
+                      print('Login com Google');
+                    },
+                    child: Image.asset(
+                      scale: 1.5,
+                      'assets/icon_google.png',
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 80.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Não tem uma conta?',
+                    textAlign: TextAlign.center,
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                  GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        'Cadastre-se',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.blue),
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
