26c003f0d73981e7dd952fcbc8f81f6f3fdec603
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-06-08 09:06:53 -0300

    refactor: separate widgets from bannerList

--- lib/presenter/widgets/home/banner_list.dart
+++ lib/presenter/widgets/home/banner_list.dart
@@ -1,10 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:r6_moovie_app/domain/entities/movie.dart';
-import 'package:r6_moovie_app/resources/app_values.dart';
-
-import '../../../domain/entities/series.dart';
+import '../../../resources/app_values.dart';
 import '../../pages/series_details_screen.dart';
+import '../home/card_image.dart';
+import '../home/card_image_text.dart';
 
 class BannerList extends StatelessWidget {
   final List<dynamic>? bannerList;
@@ -54,45 +52,12 @@ class BannerList extends StatelessWidget {
                     width: 400,
                     child: Stack(
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: CachedNetworkImage(
-                            imageUrl:
-                                "https://image.tmdb.org/t/p/w500${banner.backdropPath}",
-                            fit: BoxFit.cover,
-                            height: double.infinity,
-                            width: double.infinity,
-                            placeholder: (context, url) =>
-                                const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                        ),
+                        CardImage(media: banner),
                         Positioned(
                           bottom: 0,
                           left: 0,
                           right: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.5),
-                              borderRadius: const BorderRadius.only(
-                                bottomLeft: Radius.circular(10.0),
-                                bottomRight: Radius.circular(10.0),
-                              ),
-                            ),
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              banner is Movie
-                                  ? (banner).title.toString()
-                                  : (banner as Series).name.toString(),
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
+                          child: CardImageText(media: banner),
                         ),
                       ],
                     ),
