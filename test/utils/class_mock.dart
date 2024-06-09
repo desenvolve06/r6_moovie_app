ddd21edbf7bd9cc01d0fabe643c179f2c0425b75
arlannunesdev <arlan1.soares1@gmail.com>
2024-06-09 15:09:45 -0300

    Added: addToFavoritesUseCaseSeries test

--- test/utils/class_mock.dart
+++ test/utils/class_mock.dart
@@ -4,6 +4,7 @@ import 'package:r6_moovie_app/data/network/source/local/series/local_data_source
 import 'package:r6_moovie_app/data/network/source/remote/movie_data_source.dart';
 import 'package:r6_moovie_app/data/network/source/remote/series_data_source.dart';
 import 'package:r6_moovie_app/domain/repository/movie_repository.dart';
+import 'package:r6_moovie_app/domain/repository/series_repository.dart';
 
 class MoviesRepositoryMock extends Mock implements MoviesRepository {}
 
@@ -14,3 +15,5 @@ class MockLocalDataSourceSeries extends Mock implements LocalDataSourceSeries {}
 class MockMovieDataSource extends Mock implements MovieDataSource {}
 
 class MockLocalDataSourceMovies extends Mock implements LocalDataSourceMovies {}
+
+class SeriesRepositoryMock extends Mock implements SeriesRepository {}
