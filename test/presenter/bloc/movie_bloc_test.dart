7af83c4347573c39d61b5c6b2a148bf6bc12b195
Mauricio <mauricio.santana2010@hotmail.com>
2024-06-09 19:46:46 -0300

    /R6-123/add-test-bloc-movie
    teste para bloc movie.
    
    quem tiver dúvidas, fiz baseado nesse:
    https://pub.dev/packages/bloc_test
    
    já deixei implementado o mock de movies para teste que luciana comentou em um pr anterior (para caso ainda não tenha sido adicionado)

---
+++ test/presenter/bloc/movie_bloc_test.dart
@@ -0,0 +1,58 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:r6_moovie_app/presenter/bloc/movies/movie_bloc.dart';
+import 'package:r6_moovie_app/presenter/bloc/movies/movie_event.dart';
+import 'package:r6_moovie_app/presenter/bloc/movies/movie_state.dart';
+import 'package:r6_moovie_app/domain/usecase/movies/get_popular_movie_use_case.dart';
+
+import '../../utils/class_mock.dart';
+import '../../utils/mock_movies.dart';
+
+void main() {
+  late MoviesRepositoryMock moviesRepositoryMock;
+  late GetPopularMoviesUseCase getPopularMoviesUseCase;
+  late MovieBloc movieBloc;
+
+  setUp(() {
+    moviesRepositoryMock = MoviesRepositoryMock();
+    getPopularMoviesUseCase = GetPopularMoviesUseCase(moviesRepositoryMock);
+    movieBloc = MovieBloc(getPopularMoviesUseCase);
+  });
+
+  group('MovieBloc', () {
+    blocTest<MovieBloc, MovieState>(
+      'emits [LoadingState, LoadedSuccessState] when LoadingSuccessEvent is added and getPopularMoviesUseCase returns movies',
+      build: () {
+        when(() => moviesRepositoryMock.getPopularMovies())
+            .thenAnswer((_) async => mockMovies);
+        return movieBloc;
+      },
+      act: (bloc) => bloc.add(LoadingSuccessEvent()),
+      expect: () => [
+        LoadingState(),
+        LoadedSuccessState(movies: mockMovies),
+      ],
+      verify: (_) {
+        verify(() => moviesRepositoryMock.getPopularMovies()).called(1);
+      },
+    );
+
+    blocTest<MovieBloc, MovieState>(
+      'emits [LoadingState, ErrorState] when LoadingSuccessEvent is added and getPopularMoviesUseCase throws NetworkErrorEvent',
+      build: () {
+        when(() => moviesRepositoryMock.getPopularMovies())
+            .thenThrow(NetworkErrorEvent());
+        return movieBloc;
+      },
+      act: (bloc) => bloc.add(LoadingSuccessEvent()),
+      expect: () => [
+        LoadingState(),
+        const ErrorState(error: "Failure to get movies. Is your device online?"),
+      ],
+      verify: (_) {
+        verify(() => moviesRepositoryMock.getPopularMovies()).called(1);
+      },
+    );
+  });
+}
---
+++ test/utils/mock_movies.dart
@@ -0,0 +1,36 @@
+import 'package:r6_moovie_app/domain/entities/movie.dart';
+
+final List<Movie> mockMovies = [
+  Movie(
+    adult: false,
+    backdropPath: '/path1.jpg',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalLanguage: 'en',
+    originalTitle: 'Test Movie 1',
+    overview: 'Overview 1',
+    popularity: 100.0,
+    posterPath: '/poster1.jpg',
+    releaseDate: '2021-01-01',
+    title: 'Test Movie 1',
+    video: false,
+    voteAverage: 8.0,
+    voteCount: 1000,
+  ),
+  Movie(
+    adult: false,
+    backdropPath: '/path2.jpg',
+    genreIds: [4, 5, 6],
+    id: 2,
+    originalLanguage: 'en',
+    originalTitle: 'Test Movie 2',
+    overview: 'Overview 2',
+    popularity: 200.0,
+    posterPath: '/poster2.jpg',
+    releaseDate: '2021-02-01',
+    title: 'Test Movie 2',
+    video: false,
+    voteAverage: 7.0,
+    voteCount: 2000,
+  ),
+];
