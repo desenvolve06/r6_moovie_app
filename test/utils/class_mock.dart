d0b1adda8c7072b2c56dffdbedcf0e4639b184ce
Mauricio <mauricio.santana2010@hotmail.com>
2024-06-09 21:14:05 -0300

    Update class_mock.dart

--- test/utils/class_mock.dart
+++ test/utils/class_mock.dart
@@ -5,7 +5,6 @@ import 'package:r6_moovie_app/data/network/source/remote/movie_data_source.dart'
 import 'package:r6_moovie_app/data/network/source/remote/series_data_source.dart';
 import 'package:r6_moovie_app/domain/repository/movie_repository.dart';
 import 'package:r6_moovie_app/domain/repository/series_repository.dart';
-import 'package:r6_moovie_app/domain/repository/series_repository.dart';
 
 class MoviesRepositoryMock extends Mock implements MoviesRepository {}
 
@@ -16,9 +15,5 @@ class MockLocalDataSourceSeries extends Mock implements LocalDataSourceSeries {}
 class MockMovieDataSource extends Mock implements MovieDataSource {}
 
 class MockLocalDataSourceMovies extends Mock implements LocalDataSourceMovies {}
-
-class SeriesRepositoryMock extends Mock implements SeriesRepository {}
-
-
 
 class SeriesRepositoryMock extends Mock implements SeriesRepository {}
