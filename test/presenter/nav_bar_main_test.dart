b9235bba36147f27c0f3b3737edbf1b83025b801
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-06-15 14:05:58 -0300

    test: refactor test to verify if the navigation item is present

--- test/presenter/nav_bar_main_test.dart
+++ test/presenter/nav_bar_main_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:r6_moovie_app/presenter/widgets/home/nav_bar_main.dart';
+import 'package:r6_moovie_app/resources/app_colors.dart';
 import 'package:r6_moovie_app/resources/app_strings.dart';
+import 'package:r6_moovie_app/resources/app_values.dart';
 
 void main() {
   testWidgets('NavBarMain Widget Test', (WidgetTester tester) async {
@@ -11,12 +13,23 @@ void main() {
       ),
     ));
 
-    // Verificar se todos os itens de navegação estão presentes.
-    expect(find.text(AppStrings.home), findsOneWidget);
-    expect(find.text(AppStrings.myProfile), findsOneWidget);
-    expect(find.text(AppStrings.populars), findsOneWidget);
-    expect(find.text(AppStrings.recommended), findsOneWidget);
-    expect(find.text(AppStrings.myFavorites), findsOneWidget);
-    expect(find.text(AppStrings.about), findsOneWidget);
-     });
+    final List<String> navItems = [
+      AppStrings.home,
+      AppStrings.myProfile,
+      AppStrings.populars,
+      AppStrings.recommended,
+      AppStrings.myFavorites,
+      AppStrings.about,
+    ];
+
+    for (final item in navItems) {
+      final Finder navItemFinder = find.text(item);
+
+      // Verify if the navigation item is present
+      expect(navItemFinder, findsOneWidget);
+    }
+
+    await tester.tap(find.text(AppStrings.myFavorites));
+    await tester.pumpAndSettle();
+  });
 }
