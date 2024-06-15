a322f5a4cc3232303c8187e80b8346b4292ac4cb
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-06-15 09:27:59 -0300

    fix:Fix unit text app bar main

--- test/presenter/app_bar_main_test.dart
+++ test/presenter/app_bar_main_test.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:r6_moovie_app/presenter/widgets/home/app_bar_main.dart';
+import 'package:r6_moovie_app/presenter/widgets/home/card_text.dart';
+
+import '../stubs/stub.dart';
 
 void main() {
-  Future<void> buildApp(WidgetTester tester) async {
+  Future<void> buildApp(WidgetTester tester, dynamic media) async {
     await tester.pumpWidget(
-      const MaterialApp(
+      MaterialApp(
+        theme: ThemeData(useMaterial3: false),
         home: Scaffold(
-          appBar: AppBarMain(),
+          body: CardText(
+            media: media,
+          ),
         ),
       ),
     );
@@ -16,26 +20,77 @@ void main() {
     await tester.pumpAndSettle();
   }
 
-  group('AppBarMain Tests', () {
-    testWidgets('AppBarMain has a logo', (WidgetTester tester) async {
-      // Build the AppBarMain widget
-      await buildApp(tester);
+  group('CardTextWidget:', () {
+    testWidgets('Should render movie card text widget', (tester) async {
+      final dynamic testMedia = moviesMock;
+      await buildApp(tester, testMedia);
 
-      // Verify if the logo is present
-      expect(find.byWidgetPredicate(
-              (widget) => widget is SvgPicture && widget.semanticsLabel == 'KoruFlix Logo'
-      ), findsOneWidget);
+      final textWidget = find.descendant(
+        of: find.byType(CardText),
+        matching: find.byType(Text),
+      );
 
-      // Verify if the account icon is present
-      testWidgets('AppBarMain has a logo', (tester) async {
-      await buildApp(tester);
-      expect(find.byType(SvgPicture), findsOneWidget);
+      expect(textWidget, findsOneWidget);
+      expect((tester.widget(textWidget) as Text).data, 'Godzilla');
+
+      final textStyle = (tester.widget(textWidget) as Text).style;
+      expect(textStyle?.color, Colors.white);
+      expect(textStyle?.fontWeight, FontWeight.bold);
+
+      final container = tester.widget<Container>(find.byType(Container));
+      expect(
+        container.decoration,
+        BoxDecoration(
+          color: Colors.black.withOpacity(0.5),
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(10.0),
+            bottomRight: Radius.circular(10.0),
+          ),
+        ),
+      );
+
+      expect(container.padding, const EdgeInsets.all(8.0));
     });
 
-    testWidgets('AppBarMain has an account icon', (tester) async {
-      await buildApp(tester);
-      expect(find.byIcon(Icons.account_circle), findsOneWidget);
+    testWidgets('Should render series card text widget', (tester) async {
+      final dynamic testMedia = seriesMock;
+      await buildApp(tester, testMedia);
+
+      final textWidget = find.descendant(
+        of: find.byType(CardText),
+        matching: find.byType(Text),
+      );
+
+      expect(textWidget, findsOneWidget);
+      expect((tester.widget(textWidget) as Text).data, 'Mad');
+
+      final textStyle = (tester.widget(textWidget) as Text).style;
+      expect(textStyle?.color, Colors.white);
+      expect(textStyle?.fontWeight, FontWeight.bold);
+
+      final container = tester.widget<Container>(find.byType(Container));
+      expect(
+        container.decoration,
+        BoxDecoration(
+          color: Colors.black.withOpacity(0.5),
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(10.0),
+            bottomRight: Radius.circular(10.0),
+          ),
+        ),
+      );
+
+      expect(container.padding, const EdgeInsets.all(8.0));
     });
+
+    testWidgets('Should match golden file for series card text widget', (tester) async {
+      final dynamic testMedia = seriesMock;
+      await buildApp(tester, testMedia);
+
+      await expectLater(
+        find.byType(CardText),
+        matchesGoldenFile('golden_image/card_text_series.png'),
+      );
     });
   });
 }
