557eef5893ee98915faaf7adaeb1a452ab85c15e
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-06-15 15:46:30 -0300

    fix: fix golden Text

--- test/presenter/nav_bar_main_test.dart
+++ test/presenter/nav_bar_main_test.dart
@@ -1,39 +1,70 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:r6_moovie_app/presenter/widgets/home/nav_bar_main.dart';
 import 'package:r6_moovie_app/resources/app_colors.dart';
 import 'package:r6_moovie_app/resources/app_strings.dart';
 import 'package:r6_moovie_app/resources/app_values.dart';
 
 void main() {
-  testWidgets('NavBarMain Widget Test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
+  Widget buildMaterialApp(Widget child) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: false),
       home: Scaffold(
-        body: NavBarMain(),
+        body: child,
       ),
-    ));
+    );
+  }
 
-    final List<String> navItems = [
-      AppStrings.home,
-      AppStrings.myProfile,
-      AppStrings.populars,
-      AppStrings.recommended,
-      AppStrings.myFavorites,
-      AppStrings.about,
-    ];
-
-    for (final item in navItems) {
-      final Finder navItemFinder = find.text(item);
-
-      expect(navItemFinder, findsOneWidget);
-
-      final textWidget = tester.widget<Text>(navItemFinder);
-      expect(textWidget.style!.fontSize, equals(AppSize.s30.toDouble()));
-      expect(textWidget.style!.color, equals(AppColors.primaryColor));
-      expect(textWidget.style!.fontWeight, equals(FontWeight.bold));
-    }
-
-    await tester.tap(find.text(AppStrings.myFavorites));
-    await tester.pumpAndSettle();
+  setUpAll(() async {
+    await loadAppFonts();
   });
+
+  group('NavBarMain Tests', () {
+    testWidgets('Should render nav Items texts', (WidgetTester tester) async {
+      await tester.pumpWidget(buildMaterialApp(const NavBarMain()));
+
+      final List<String> navItems = [
+        AppStrings.home,
+        AppStrings.myProfile,
+        AppStrings.populars,
+        AppStrings.recommended,
+        AppStrings.myFavorites,
+        AppStrings.about,
+      ];
+
+      for (final item in navItems) {
+        verifyNavItemStyle(tester, item, AppSize.s30.toDouble(), AppColors.primaryColor, FontWeight.bold);
+      }
+    });
+
+    testGoldens('Should render Golden Test nav item ', (WidgetTester tester) async {
+      await tester.pumpWidgetBuilder(buildMaterialApp(const NavBarMain()));
+      await tester.pumpAndSettle();
+
+      await expectLater(
+        find.byType(NavBarMain),
+        matchesGoldenFile('golden_image/nav_bar_main.png'),
+      );
+    });
+  });
+}
+
+void verifyNavItemStyle(
+    WidgetTester tester,
+    String item,
+    double expectedFontSize,
+    Color expectedColor,
+    FontWeight expectedFontWeight,
+    ) {
+  final Finder navItemFinder = find.text(item);
+
+  expect(navItemFinder, findsOneWidget);
+
+  final textWidget = tester.widget<Text>(navItemFinder);
+  expect(textWidget.style!.fontSize, equals(expectedFontSize));
+  expect(textWidget.style!.color, equals(expectedColor));
+  expect(textWidget.style!.fontWeight, equals(expectedFontWeight));
 }
